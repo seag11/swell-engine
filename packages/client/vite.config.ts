cb6434731b0dd93b6f634b5867cc0aa469694eaf
seag11 <seag11@users.noreply.github.com>
2026-07-16 13:47:47 -0700

    chore: drop debugger statements

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  esbuild: {
+    drop: ['debugger'],
+  },
   server: {
     port: 5173,
     proxy: {
